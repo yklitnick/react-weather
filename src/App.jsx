d839401945a9b3da0509df8dcb05c5ca127fa5ee
yklitnick <yitzchokyacovklitnick@gmail.com>
2025-12-26 15:32:06 -0500

    Feat: add weather logic and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,89 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
-    const [count, setCount] = useState(0);
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
+function App() {
+    const [weather, setWeather] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    const dateBuilder = () => {
+        let d = new Date();
+        let months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ];
+        let days = [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+        ];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day} ${date} ${month} ${year}`;
+    };
+
+    const fetchWeather = async (e) => {
+        if (e.key === 'Enter') {
+            const response = await fetch(
+                `${BASE_URL}weather?q=${e.target.value}&appid=${API_KEY}&units=imperial`
+            );
+            const data = await response.json();
+            setWeather(data);
+            setLoading(false);
+        }
+    };
     return (
         <>
-            <div>
-                <a href='https://vite.dev' target='_blank'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
+            <main id='main'>
+                <div className='search-box'>
+                    <input
+                        type='text'
+                        className='search-bar'
+                        placeholder='Search...'
+                        onKeyDown={fetchWeather}
                     />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
+                </div>
+                {typeof weather.main != 'undefined' && !loading && (
+                    <div className='weather-wrap'>
+                        <div className='location-box'>
+                            <div className='location'>
+                                {weather.name}, {weather.sys.country}
+                            </div>
+                            <div className='date'>
+                                {dateBuilder(new Date())}
+                            </div>
+                        </div>
+                        <div className='weather-box'>
+                            <div className='temp'>
+                                {Math.round(weather.main.temp)}°f
+                            </div>
+                            <div className='weather'>
+                                {weather.weather[0].main}
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </main>
         </>
     );
 }
